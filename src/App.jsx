f46373c2e20c8ff25e1e4f297ee6f7a95cb685ac
Saikalyan Akunuri <saikalyan.akunuri@gmail.com>
2024-06-19 11:40:09 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import Highlights from './components/highlights';
+
+const App = () => {
+  
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+
+    </main>
+  )
+}
+
+export default App
